d07b1e4345187d0fc4a24e16fe99bb338892ed91
ninhnam <ninhnamyb2001@gmail.com>
2024-03-29 16:01:54 +0700

    setup

--- src/app/[lang]/(primary)/hoc-vien-duc-anh/page.jsx
+++ src/app/[lang]/(primary)/hoc-vien-duc-anh/page.jsx
@@ -1,5 +1,12 @@
 export default function page() {
   return (
-    <div className='w-full h-screen title pt-[9.69rem]'>HỌC VIÊN ĐỨC ANH</div>
+    <div className='container'>
+      <h1 className='w-full title pt-[9.69rem] pb-[3rem] text-primary-50'>
+        Học viên Đức Anh
+      </h1>
+      color: var(--PRIMARY-50, #2B46A8); font-family: "Plus Jakarta Sans";
+      font-size: 3rem; font-style: normal; font-weight: 800; line-height: 120%;
+      /* 3.6rem */ letter-spacing: -0.03rem;
+    </div>
   )
 }
