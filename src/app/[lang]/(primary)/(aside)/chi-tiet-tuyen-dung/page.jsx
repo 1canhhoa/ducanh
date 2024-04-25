32e63c415e96ddefdc9b97ef605b3d7d0485cb19
1canhhoa <nhamvanhien31102002@gmail.com>
2024-04-25 17:19:05 +0700

    hshshsh

--- src/app/[lang]/(primary)/(aside)/chi-tiet-tuyen-dung/page.jsx
+++ src/app/[lang]/(primary)/(aside)/chi-tiet-tuyen-dung/page.jsx
@@ -5,7 +5,7 @@ import RecruitmentDetailsIndex from '@/sections/recruitmentDetails/RecruitmentDe
 const RecruitmentDetailsPage = async ({params}) => {
   const t = await getDictionary(params.lang)
   const res = await getData(
-    'https://duc-anh.okhub-tech.com/wp-json/wp/v2/recruit/31',
+    '/wp-json/wp/v2/recruit/31',
   )
   return (
     <RecruitmentDetailsIndex
