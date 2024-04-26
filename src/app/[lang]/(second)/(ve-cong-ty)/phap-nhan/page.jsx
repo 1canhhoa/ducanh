155875228bd2c970b2a6dd5851e1f9cee0ff5ece
1canhhoa <nhamvanhien31102002@gmail.com>
2024-04-26 10:29:50 +0700

    ppp

--- src/app/[lang]/(second)/(ve-cong-ty)/phap-nhan/page.jsx
+++ src/app/[lang]/(second)/(ve-cong-ty)/phap-nhan/page.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
-const page = () => {
+import Banner from '@/sections/about-us/Banner'
+import { getDictionary } from '@/app/[lang]/dictionaries.js'
+import Ceo from '@/sections/about-us/Ceo'
+import Business from '@/sections/about-us/Business'
+import TonChi from '@/sections/about-us/TonChi'
+import Achievement from '@/sections/about-us/Achievement'
+import Office from '@/sections/about-us/Office.jsx'
+import Staff from '@/sections/about-us/Staff'
+import WorkTime from '@/sections/about-us/WorkTime'
+import Timestamp from '@/sections/about-us/Timestamp'
+const page = async ({ params, searchParams }) => {
+  const { about_us } = await getDictionary(params.lang)
+  const { viewport } = searchParams
+  const isMobile = viewport?.includes('mobile')
   return (
-    <div>
-      phap-nhan
-    </div>
+    <Banner t={about_us} isMobile={isMobile} />
+
   )
 }
 
--- src/components/SocialAction/SocialAction.jsx
+++ src/components/SocialAction/SocialAction.jsx
@@ -1,8 +1,29 @@
+"use client"
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import FormDangKyTuVan from '@/components/form-dang-ki-tu-van/FormDangKyTuVan.jsx'
+import useClickOutSide from '@/hooks/useClickOutSide'
 const SocialAction = () => {
+  const [active, setActive] = useState(-1)
+  const [sideRef, isOutSide] = useClickOutSide();
+  useEffect(() => {
+    if (isOutSide) {
+      setActive(-1)
+      console.log('click been ngoai');
+    } else {
+      // Xử lý khi click bên trong thẻ <div>
+      console.log('click been trong');
+
+    }
+  }, [isOutSide]);
   return (
     <div className=" fixed z-[50] xmd:bottom-[5.2rem] bottom-[3.2rem] xmd:right-[0.5rem] right-[1.5rem] flex flex-col xmd:space-y-[0.5rem] space-y-[0.7rem]">
-      <Image priority alt="ảnh" src={'/images/homepage/mess4.png'} width={56} height={56} className="w-full h-full xmd:size-[2.5rem] size-[3.5rem]" />
+      <Image onClick={() => setActive(1)} priority alt="ảnh" src={'/images/homepage/mess4.png'} width={56} height={56} className="w-full h-full cursor-pointer xmd:size-[2.5rem] size-[3.5rem]" />
+      {active === 1 &&
+        <div ref={sideRef} className='bd fixed w-[21.875rem] bottom-[10rem] right-[10rem]'>
+          <FormDangKyTuVan />
+        </div>
+      }
       <Image priority alt="ảnh" src={'/images/homepage/mess.png'} width={56} height={56} className="w-full h-full  xmd:size-[2.5rem] size-[3.5rem]" />
       <Image priority alt="ảnh" src={'/images/homepage/mess3.png'} width={56} height={56} className="w-full h-full xmd:size-[2.5rem] size-[3.5rem]" />
       <Image priority alt="ảnh" src={'/images/homepage/mess2.png'} width={56} height={56} className="w-full h-full xmd:size-[2.5rem] size-[3.5rem]" />
